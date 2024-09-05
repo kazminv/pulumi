6f5607b1e3f2303164ebbe70bd62871bcd009aae
Vasilii Kazmin <vasyakazmin171@yandex.ru>
2024-09-05 13:00:22 +0100

    added a comment:

--- __main__.py
+++ __main__.py
@@ -3,7 +3,7 @@ from instance import ec2_instance
 from instance import bucket
 from vpc import vpc
 from vpc import ami_actual
-
+#new
 # Export the IDs of the created resources
 pulumi.export("instance_id", ec2_instance.id)
 pulumi.export("bucket_name", bucket.id)
